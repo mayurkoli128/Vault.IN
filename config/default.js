02d535830c600b5dd95845e0f15f2ca31a381d9f
Mayur Koli <kolimayur566@gmail.com>
2021-07-31 13:07:53 +0530

    minor changes in UI

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ module.exports = {
         connectionLimit: 10,
         host: process.env.DATABASE_HOST || 'localhost',
         user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || 'root128',
+        password: process.env.DATABASE_PASSWORD || 'root',
         database: process.env.DATABASE_NAME || 'VaultIN'
     },
     port: process.env.PORT || 8080
