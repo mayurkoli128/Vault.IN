72edf107ce8a671c9740f0f5e5eeb95335213958
Mayur Koli <kolimayur566@gmail.com>
2021-07-31 14:16:16 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('express-async-errors');
 require('dotenv').config();
 
 require('./startup/routes')(app, express);
+require('./startup/production')(app) ;
 
 app.listen(process.env.PORT || 8080, ()=> {
     console.log('Listening on PORT: 8080');
